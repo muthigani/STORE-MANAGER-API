c409f0a6011d1cf9903ffdc79620222133f39377
muthigani <muthigan@gmail.com>
2018-10-20 15:57:09 +0300

    [ft-#161355665]update on views

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,83 +1,160 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import jsonify, make_response
+from flask_restful import Resource, reqparse
 
-orders = []
+# Sales Endpoints
+
+sales = []
 
 
-class OrderList(Resource):
+class SalesList(Resource):
     def get(self):
-        return orders, 200
+        return make_response(jsonify(
+            {
+                'status':"OK",
+                'Message':"success",
+                'My Sales': sales
+            }),200)
 
 
-class Order(Resource):
-    def get(self, orderid):
-        for order in orders:
-            if (orderid == order["orderid"]):
-                return order, 200
+class Sales(Resource):
+
+    def get(self, salesid):
+        for sale in sales:
+            if salesid == sale["salesid"]:
+                return make_response(jsonify(
+                    {
+                        'status':"OK",
+                        'Message':"success",
+                        'My Sale': sale
+                    }),200)
             return "User not found", 404
 
-    def post(self, orderid):
+    def post(self, salesid):
         parser = reqparse.RequestParser()
-        parser.add_argument("food")
+        parser.add_argument("productname")
         parser.add_argument("clientname")
-        parser.add_argument("location")
-        parser.add_argument("phone")
+        parser.add_argument("phonenumber")
         parser.add_argument("quantity")
         parser.add_argument("price")
-        parser.add_argument("orderstat")
         args = parser.parse_args()
 
-        for order in orders:
-            if (orderid == order["orderid"]):
-                return "order with ID number {} already exists".format(orderid), 400
 
-        order = {
-            "orderid": orderid,
-            "food": args["food"],
+        for sale in sales:
+            if salesid == sale["salesid"]:
+                return "order with ID number {} already exists".format(salesid), 400
+
+        sale = {
+            "salesid": salesid,
+            "productname": args["productname"],
             "clientname": args["clientname"],
-            "location": args["location"],
-            "phone": args["phone"],
+            "phonenumber": args["phonenumber"],
             "quantity": args["quantity"],
             "price": args["price"],
-            "orderstat": args["orderstat"],
+
 
         }
-        orders.append(order)
-        return order, 201
+        sales.append(sale)
+        return make_response(jsonify(
+            {
+                'status':"OK",
+                'Message':"Posted successfully",
+                'My Sale': sale
+            }),201)
 
-    def put(self, orderid):
+    def put(self, salesid):
         parser = reqparse.RequestParser()
-        parser.add_argument("food")
+        parser.add_argument("productname")
         parser.add_argument("clientname")
-        parser.add_argument("location")
-        parser.add_argument("phone")
+        parser.add_argument("phonenumber")
         parser.add_argument("quantity")
         parser.add_argument("price")
-        parser.add_argument("orderstat")
         args = parser.parse_args()
 
-        for order in orders:
-            if (orderid == order["orderid"]):
-                order["food"] = args["food"]
-                order["clientname"] = args["clientname"]
-                order["location"] = args["location"]
-                order["phone"] = args["phone"]
-                order["quantity"] = args["quantity"]
-                order["price"] = args["price"]
-                order["orderstat"] = args["orderstat"]
-                return order, 200
+        for sale in sales:
+            if (salesid == sale["salesid"]):
+                sale["productname"] = args["productname"]
+                sale["clientname"] = args["clientname"]
+                sale["phonenumber"] = args["phonenumber"]
+                sale["quantity"] = args["quantity"]
+                sale["price"] = args["price"]
 
-        order = {
-            "orderid": orderid,
-            "food": args["food"],
-            "clientname": args["clientname"],
-            "location": args["location"],
-            "phone": args["phone"],
+
+        sale = {
+                "salesid": salesid,
+                "productname": args["productname"],
+                "clientname": args["clientname"],
+                "phonenumber": args["phonenumber"],
+                "quantity": args["quantity"],
+                "price": args["price"],
+              }
+
+        sales.append(sale)
+        return sale, 201
+
+# Product Endpoint
+
+products = []
+
+
+class ProductList(Resource):
+    def get(self):
+        return products, 200
+
+
+class Product(Resource):
+    def get(self, productid):
+        for product in products:
+            if productid == product["productid"]:
+                return product, 200
+            return "User not found", 404
+
+    def post(self, productid):
+        parser = reqparse.RequestParser()
+        parser.add_argument("productname")
+        parser.add_argument("quantity")
+        parser.add_argument("price")
+        parser.add_argument("status")
+        args = parser.parse_args()
+
+        for product in products:
+            if productid == product["productid"]:
+                return "product with ID number {} already exists".format(productid), 400
+
+        product = {
+            "productid": productid,
+            "productname": args["productname"],
             "quantity": args["quantity"],
             "price": args["price"],
-            "orderstat": args["orderstat"],
+            "status": args["status"],
+
+        }
+        products.append(product)
+        return product, 201
+
+    def put(self, productid):
+        parser = reqparse.RequestParser()
+        parser.add_argument("productname")
+        parser.add_argument("quantity")
+        parser.add_argument("price")
+        parser.add_argument("status")
+        args = parser.parse_args()
+
+        for product in products:
+            if productid == product["productid"]:
+                product["productname"] = args["productname"]
+                product["quantity"] = args["quantity"]
+                product["price"] = args["price"]
+                product["status"] = args["status"]
+                return product, 200
+
+        product = {
+            "productid": productid,
+            "productname": args["productname"],
+            "quantity": args["quantity"],
+            "price": args["price"],
+            "status": args["status"],
 
         }
 
-        orders.append(order)
-        return order, 201
+        products.append(product)
+        return product, 201
