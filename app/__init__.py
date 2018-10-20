511a6e655e2d6fe6c3963c44e90d7bccc54d7e90
muthigani <muthigan@gmail.com>
2018-10-20 21:58:24 +0300

    [bg-#161339341] sorting errors on views

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask, Blueprint
-from config import api_config
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from instance.config import app_config
+# from app.api.v1.views.users import BLACKLIST
 
 
-def create_app(config_filename):
-    app = Flask(__name__)
-
-    #app.config.from_object('config')
-
-
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config['development'])
+    
     from app.api.v1 import version1 as v1
+
+    # register the blueprint
     app.register_blueprint(v1)
 
+    app.config['JWT_SECRET_KEY'] = 'makorokocho'
+    jwt = JWTManager(app)
 
 
     return app
-
--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,16 +1,20 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from instance.config import app_config
+from app.api.v1.views.views import ProductsList, Product, SalesList, Sale
+from app.api.v1.views.users import Register, Login 
 
-from app.api.v1.views import Sales, SalesList, Product, ProductList
-
-version1 = Blueprint('api',__name__, url_prefix ='/api/v1')
+# obtain the blueprint by initialising
+version1 = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
-api.add_resource(Sales, '/sales/<int:salesid>')
+
+
+# specific endpoints
 api.add_resource(SalesList, '/sales')
-
-api.add_resource(Product, '/products/<int:productid>')
-api.add_resource(ProductList, '/products')
-
-
-
+api.add_resource(Sale, '/sales/<salesid>')
+api.add_resource(ProductsList, '/products')
+api.add_resource(Product, '/products/<productid>')
+api.add_resource(Register, '/authorization/register')
+api.add_resource(Login, '/authorization/login')
